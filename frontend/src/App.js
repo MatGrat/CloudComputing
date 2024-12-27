f2647aa8244f5e9d888f16873edc612a02336a79
MatGrat <mathis.grabert@icloud.com>
2024-12-27 14:24:58 +0100

    add nginx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,24 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
+    <main>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
         <Navbar.Brand href="#home">HomiShoppi</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Account</Nav.Link>
             <Nav.Link href="#link">ShopWagen</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+    </main>
   );
 }
 
